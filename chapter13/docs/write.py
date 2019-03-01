964e308db09d54e86f6c272765dfaeab6ac914c8
Janak Soni <janak.soni@infostretch.com>
2019-02-28 17:51:35 -0800

    adds a paragraph with the text Hello world! in the Title style

--- chapter13/docs/write.py
+++ chapter13/docs/write.py
@@ -1,5 +1,9 @@
 import docx
 
 doc = docx.Document()
-doc.add_paragraph('Hello world!')
-doc.save('helloworld.docx')
+doc.add_paragraph('Hello world!', 'Title')
+
+paraObj1 = doc.add_paragraph('second para')
+paraObj2 = doc.add_paragraph('another para')
+paraObj1.add_run(' This text is being added to the second paragraph.')
+doc.save('multipleParagraphs.docx')
